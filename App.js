cbd3885abaa83a34ddbecb2b8fe862e7533933c8
minho0078 <minho0078@gmail.com>
2018-06-18 17:25:00 +0900

    first commit!!

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>this is minho's react native app </Text>
+        <Text style={styles.greeting}>Hi!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    color:"orange"
+  }
+});
